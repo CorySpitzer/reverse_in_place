e1d6109dde3bb0a7445336a756a093b01f07f51c
Cory Spitzer <coryspitzer@gmail.com>
2016-02-15 15:09:09 -0500

    Handle a single-entry array

--- reverse_spec.rb
+++ reverse_spec.rb
@@ -5,4 +5,8 @@ describe '#reverse' do
   it 'handles an empty array' do
     expect(reverse []).to eq []
   end
+
+  it 'handles single-entry array' do
+    expect(reverse [3]).to eq [3]
+  end
 end
