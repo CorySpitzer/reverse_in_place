73b195c8c6b1135c34279cd9c7d71e69a527c368
Cory Spitzer <coryspitzer@gmail.com>
2016-02-15 15:32:24 -0500

    Handle double-entry array

--- reverse.rb
+++ reverse.rb
@@ -1,3 +1,15 @@
 def reverse(list)
+  if list.length > 1
+    temporary_entry = list[1]
+    # puts "temporary_entry: #{temporary_entry}"
+    list[1] = list[0]
+    # puts "list[1]: #{list[1]}"
+    list[0] = temporary_entry
+    # puts "list[0]: #{list[0]}"
+    # puts "list: #{list}"
+  end
+  # puts "list: #{list}"
   list
 end
+
+# reverse ['a', 'b']
--- reverse_spec.rb
+++ reverse_spec.rb
@@ -9,4 +9,8 @@ describe '#reverse' do
   it 'handles single-entry array' do
     expect(reverse [3]).to eq [3]
   end
+
+  it 'handles double-entry array' do
+    expect(reverse [3, 4]).to eq [4,3]
+  end
 end
